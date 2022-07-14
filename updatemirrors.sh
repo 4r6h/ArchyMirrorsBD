51999f8fb0b37a9d13d713dbf2cef148c4bb253c
4r6h <108066623+4r6h@users.noreply.github.com>
2022-07-15 00:10:27 +0600

    Update updatemirrors.sh

--- updatemirrors.sh
+++ updatemirrors.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/env bash
 #reflector --latest 200 --protocol http,https --sort rate --save $HOME/mirrorlist
 sudo reflector --latest 200 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
