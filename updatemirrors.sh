d22eb4b476a0ed8010f1f7fc6f07675cb7ae85a2
4r6h <108066623+4r6h@users.noreply.github.com>
2022-07-20 12:10:56 +0600

    Update updatemirrors.sh

--- updatemirrors.sh
+++ updatemirrors.sh
@@ -3,3 +3,4 @@
 sudo reflector --latest 200 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
 sudo sed -i '/xeonbd/d' /etc/pacman.d/mirrorlist
 sudo sed -i '1 i Server = http://mirror.xeonbd.com/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
+sed '/^Server=.*/i Done'
