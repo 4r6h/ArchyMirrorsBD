1ae0d16b9df2771470ea9904d6770977734e3d5a
4r6h <108066623+4r6h@users.noreply.github.com>
2022-07-23 16:57:02 +0600

    Update updatemirrors.sh

--- updatemirrors.sh
+++ updatemirrors.sh
@@ -2,6 +2,7 @@
 #reflector --latest 200 --protocol http,https --sort rate --save $HOME/mirrorlist
 sudo reflector --latest 200 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
 sudo sed -i '/xeonbd/d' /etc/pacman.d/mirrorlist #Deletes the Bangladesh Mirror
+sudo sed -i 's|^$|Server = http://mirror.xeonbd.com/archlinux/$repo/os/$arch|' /etc/pacman.d/mirrorlist #Adds Bangladesh Mirror
 
 #sed -i 's#{{find}}#{{replace}}#g' {{filename}}
 
