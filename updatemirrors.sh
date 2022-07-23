b76cf6f543c16c9d33566a0087ab21f9f891a039
4r6h <108066623+4r6h@users.noreply.github.com>
2022-07-23 18:28:07 +0600

    Update updatemirrors.sh

--- updatemirrors.sh
+++ updatemirrors.sh
@@ -1,9 +1,26 @@
 #!/bin/env bash
-sudo reflector --latest 200 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
+#######################################################################################################################################################
+
+Bangladesh='Server = http://mirror.xeonbd.com/archlinux/$repo/os/$arch' 			      #Defining Bangladesh Mirror.
+
+mirrorlist='/etc/pacman.d/mirrorlist'								                                #Path to Pacman Mirrorlist.
+
+#mirrorlist='mirrorlist'
+
+sudo reflector --latest 200 --protocol http,https --sort rate --save $mirrorlist 		#Rates Mirrors For Best Speed.
 wait
-sudo sed -i '/xeonbd/d' /etc/pacman.d/mirrorlist #Deletes the Bangladesh Mirror
+
+sudo sed -i '/xeonbd/d' $mirrorlist 									                              #Deletes the Bangladesh Mirror.
 wait
-sudo sed -i '/^$/a Server = http://mirror.xeonbd.com/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist #Adds Bangladesh Mirror after blank line
+
+sed -i "/^$/a $Bangladesh" $mirrorlist								                              #Adds Bangladesh Mirror Again at the First Position.
+#######################################################################################################################################################
+
+#sudo reflector --latest 200 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
+#wait
+#sudo sed -i '/xeonbd/d' /etc/pacman.d/mirrorlist #Deletes the Bangladesh Mirror
+#wait
+#sudo sed -i '/^$/a Server = http://mirror.xeonbd.com/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist #Adds Bangladesh Mirror after blank line
 
 #reflector --latest 200 --protocol http,https --sort rate --save $HOME/mirrorlist
 
