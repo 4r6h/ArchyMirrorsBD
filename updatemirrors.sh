b5f525421a85d204d28b17c2bef8af8d7705355e
4r6h <hrahat023@gmail.com>
2022-08-15 13:33:18 +0000

    Better error hanlaling and better error masseges

--- updatemirrors.sh
+++ updatemirrors.sh
@@ -2,31 +2,6 @@
 
 shopt -s extglob
 
-#######################################################################################################################################################
-
-# Defining who can run the script.
-
-if [ $(whoami) = "root" ]; then
-	
-	if [ ! -x /bin/reflactor ]; then
-
-		pacman -S --noconfirm --needed reflector
-	fi
-	
-	while true 
-		do
-		echo 'How many parallel downloadings do you want?'
-		echo 'select between 6 or 12'
-		read -r -p "Insert a number :" INPUT
-	if [ $INPUT -gt "12" ]; then
-		echo "Don't use more than 12 downloadings"
-	elif [ $INPUT -eq "0" ]; then
-		echo "Don't use 0 downloadings"
-	elif [ $INPUT -lt "5" ]; then
-		echo "Don't use less than 5 downloadings"
-	else
-		case $INPUT in
-			( +([[:digit:]]) )
 
 # Defining Bangladesh Mirror.
 
@@ -35,6 +10,9 @@ if [ $(whoami) = "root" ]; then
 # Path to Pacman Mirrorlist.
 
 	mirrorlist='/etc/pacman.d/mirrorlist' 
+
+
+updatemirrors () {
 
 # Rates Mirrors For Best Speed.
 
@@ -49,28 +27,95 @@ if [ $(whoami) = "root" ]; then
 	sed -i.4r6h "1,/^Server/ {/^Server/i\
 	$Bangladesh
 	}" $mirrorlist
+}
+
+
+on_parallel () {
 
 # Add parallel downloading
-	
-	sed -i.4r6h "s/^ParallelDownloads.*/ParallelDownloads = $INPUT/" /etc/pacman.conf
-	pacman -Syy --noconfirm
-break
-			;;
 
-			*)	
-				echo "You should give a whole number"
-			;;
-		esac
-	fi
+while true 
+do
+	echo -e "\n How many parallel downloadings do you want?\n select between 6 to 12 or press enter for default\n"
+
+		read -r -p " Insert a number: " INPUT
+		
+				case "$INPUT" in
+					
+					[0-4])
+						echo -e "\n Don't use less than 5 downloadings! \n" ;;
+					
+					
+					1[3-*])
+						echo -e "\n Don't use more than 12 downloadings! \n" ;;
+	
+					
+					[5-9]|1[0-2])
+
+						sed -i.4r6h "s/^ParallelDownloads.*/ParallelDownloads = $INPUT/" /etc/pacman.conf
+
+											break ;;
+					$ENTER)
+					
+						sed -i.4r6h "s/^ParallelDownloads.*/ParallelDownloads = 5/" /etc/pacman.conf
+											
+											break ;;
+					( +([[:digit:]]) )
+
+						echo -e "\n invalid $INPUT is not acceptable! \n" ;;
+
+					*) 
+						echo -e "\n Error! You should give a whole number!\n" ;;
+				esac
+done
+
+}
+
+
+# Defining who can run the script.
+
+if [ $(whoami) = "root" ]; then
+	
+		if [ ! -x /bin/reflactor ]; then
+
+			pacman -S --noconfirm --needed reflector
+		fi
+	
+	while true 
+	do
+
+		echo -e "\n Do you want to add parallel downloadings?\n"
+	
+		read -r -p " Type [Y/n] then press enter. default is (Y): " para
+		
+			case "$para" in
+				[Yy]|[Yy][Ee][Ss]|$ENTER)
+				
+					on_parallel
+					updatemirrors  
+							break ;;
+
+				[Nn]|[Nn][Oo])
+				
+					updatemirrors 
+							break ;;
+				*)
+					echo -e "\n $para is invalid input.... \n"
+			esac
 	done
 else
-
-echo "
-	__________________________________________
-	_____________PLEASE RUN AS ROOT___________
-	__________________________________________
-     "
+		echo -e	"\n
+			!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			------------PLEASE RUN AS ROOT------------
+			!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+			\n"
 fi
+
+
+
+
+
+
 #######################################################################################################################################################
 
 #sudo sed -i "/^$/a $Bangladesh" $mirrorlist #Adds Bangladesh Mirror Again at the First Position.
