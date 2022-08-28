67889287ebbed9ae8b9fe55b49c80a2a3b470294
4r6h <hrahat023@gmail.com>
2022-08-28 18:28:56 +0000

    Added Colors

--- updatemirrors.sh
+++ updatemirrors.sh
@@ -7,6 +7,30 @@
 
 shopt -s extglob
 
+   Off='\033[0m'	     # Text Reset
+
+   # Regular Colors
+   Black='\033[0;30m'        # Black
+   Red='\033[0;31m'          # Red
+   Green='\033[0;32m'        # Green
+   Yellow='\033[0;33m'       # Yellow
+   Blue='\033[0;34m'         # Blue
+   Purple='\033[0;35m'       # Purple
+   Cyan='\033[0;36m'         # Cyan
+   White='\033[0;37m'        # White
+
+   # Bold
+   BBlack='\033[1;30m'       # Black
+   BRed='\033[1;31m'         # Red
+   BGreen='\033[1;32m'       # Green
+   BYellow='\033[1;33m'      # Yellow
+   BBlue='\033[1;34m'        # Blue
+   BPurple='\033[1;35m'      # Purple
+   BCyan='\033[1;36m'        # Cyan
+   BWhite='\033[1;37m'       # White
+
+
+
 # Defining Bangladesh Mirror.
 
 	Bangladesh='Server = http://mirror.xeonbd.com/archlinux/$repo/os/$arch' 
@@ -40,30 +64,34 @@ on_parallel() {
 
 while true 
 do
-	echo -e "\n How many parallel downloadings do you want?\n select between 6 to 12 or press enter for default\n"
+	echo -e "${BGreen}\n How many parallel downloadings do you want?\n select between 6 to 12 or press enter for default\n"
 
-		read -r -p " Insert a number: " INPUT
+		read -r -p " Insert a number: [0m" INPUT
 		
 				case "$INPUT" in
 					
 					$ENTER)	
 						sed -i.4r6h "s/^ParallelDownloads.*/ParallelDownloads = 5/" /etc/pacman.conf
+
+					echo -e "${BGreen}\n Upgrading Mirrors also enabled 5 parallel downloadings.\n"	
 											
 											break ;;
 					[0-4])
-						echo -e "\n Don't use less than 5 downloadings! \n" ;;
+						echo -e "${BRed}\n Don't use less than 5 downloadings! \n" ;;
 					
 					[5-9]|1[0-2])
 
 						sed -i.4r6h "s/^ParallelDownloads.*/ParallelDownloads = $INPUT/" /etc/pacman.conf
 
+					echo -e "${BGreen}\n Upgrading Mirrors also enabled $INPUT parallel downloadings.\n"	
+
 											break ;;
 					( +([[:digit:]]) )
 
-						echo -e "\n Don't use more than 12 downloadings! \n" ;;
+						echo -e "${BRed}\n Don't use more than 12 downloadings! \n" ;;
 
 					*) 
-						echo -e "\n Error! You should give a whole number!\n" ;;
+						echo -e "${BRed}\n Error! You should give a whole number!\n" ;;
 				esac
 done
 
@@ -83,31 +111,34 @@ if [ $(whoami) = "root" ]; then
 	while true 
 	do
 
-		echo -e "\n Do you want to add parallel downloadings?\n"
+		echo -e "${BGreen}\n Do you want to add parallel downloadings?\n"
 	
-		read -r -p " Type [Y/n] then press enter. default is (Y): " para
+		read -r -p " Type [Y/n] then press enter. default is (Y): [0m" para
 		
 			case "$para" in
 				[Yy]|[Yy][Ee][Ss]|$ENTER)
-				
+					
 					on_parallel
+
 					updatemirrors  
 							break ;;
 
 				[Nn]|[Nn][Oo])
 				
+					echo -e "${BGreen}\n Upgrading Mirrors without parallel downloadings.\n"	
+				
 					updatemirrors 
 							break ;;
 				*)
-					echo -e "\n $para is invalid input.... \n"
+					echo -e "${BRed}\n $para is invalid input....\n${Off}"
 			esac
 	done
 else
-		echo -e	"\n
-			!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-			------------PLEASE RUN AS ROOT------------
-			!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-			\n"
+	echo -e	"${BRed}\n
+		!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		------------PLEASE RUN AS ROOT------------
+		!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		\n ${Off}"
 fi
 
 }
